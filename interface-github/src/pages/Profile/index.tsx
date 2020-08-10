6b58d73b594a7e60d5794c71a3671d94a154fd27
Rafael <rafael-rafa2.1@hotmail.com>
2020-08-10 17:53:51 -0300

    Page Profile

--- interface-github/src/pages/Profile/index.tsx
+++ interface-github/src/pages/Profile/index.tsx
@@ -1,9 +1,80 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide, Repos, CalendarHeading, RepoIcon, Tab } from './styles';
+
+import ProfileData from '../../components/ProfileData';
+import RepoCard from '../../components/RepoCard';
+import RandomCalendar from '../../components/RandomCalendar';
+// import { RepoIcon } from '../../components/RepoCard/styles';
 
 const Profile: React.FC = () => {
-  return <div />;
+  const TabContent = () => (
+    <div className="content">
+      <RepoIcon />
+      <span className="label">Repositories</span>
+      <span className="number">26</span>
+    </div>
+  )
+
+  return (
+    <Container>
+      <Tab className="desktop">
+        <div className="wrapper">
+          <span className="offset" />
+          <TabContent />
+        </div>
+
+        <span className="line" />
+      </Tab>
+
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username={'guilhermerodz'}
+            name={'Guilherme Rodz'}
+            avatarURL={'https://avatars0.githubusercontent.com/u/10366880?s=460&u=ae99daa8dbb052dfcd0048862ab87007f677e6ed&v=4'}
+            followers={887}
+            followwing={7}
+            company={'Rocketseat'}
+            location={'São Paulo, Brazil'}
+            email={'emailqualquer@gmail.com'}
+            blog={undefined}
+          />
+        </LeftSide>
+
+        <RightSide>
+          <Tab className="mobile">
+            <TabContent />
+            <span className="line" />
+          </Tab>
+
+          <Repos>
+            <h2>Random repos</h2>
+
+            <div>
+              {[1, 2, 3, 4, 5, 6].map(n => (
+                <RepoCard
+                  key={n}
+                  username={'guilhermerodz'}
+                  reponame={'youtubo-content'}
+                  description={'Contains all of my YouTube lessons code'}
+                  language={n%3 === 0 ? 'JavaScript' : 'TypeScript'}
+                  stars={8}
+                  forks={4}
+                />
+              ))}
+            </div>
+          </Repos>
+
+          <CalendarHeading>
+            Random calendar (do not represent actual data)
+          </CalendarHeading>
+
+          <RandomCalendar />
+        </RightSide>
+      </Main>
+    </Container>
+  );
 }
 
 export default Profile;
