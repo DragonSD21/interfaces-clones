6e920d0cafab6fdf6a135316f416b902fd31c34b
Rafael <rafael-rafa2.1@hotmail.com>
2020-08-13 09:57:19 -0300

    Page Repo + Navegação + Footer

--- interface-github/src/pages/Profile/index.tsx
+++ interface-github/src/pages/Profile/index.tsx
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import { Container, Main, LeftSide, RightSide, Repos, CalendarHeading, RepoIcon, Tab } from './styles';
 
 import ProfileData from '../../components/ProfileData';
 import RepoCard from '../../components/RepoCard';
 import RandomCalendar from '../../components/RandomCalendar';
-// import { RepoIcon } from '../../components/RepoCard/styles';
+
+import { APIUser, APIRepo } from '../../@types';
+
+interface Data {
+  user?: APIUser;
+  repos?: APIRepo[];
+  error?: string;
+}
 
 const Profile: React.FC = () => {
+  const { username = 'guilhermerodz' } = useParams();
+  const [data, setData] = useState<Data>();
+
+  useEffect(() => {
+      Promise.all([
+        fetch(`https://api.github.com/users/${username}`),
+        fetch(`https://api.github.com/users/${username}/repos`),
+    ]).then(async responses => {
+      const [userResponse, reposResponse] = responses;
+
+      if(userResponse.status === 404) {
+        setData({ error: 'User not found!' });
+        return;
+      }
+
+      const user = await userResponse.json();
+      const repos = await reposResponse.json();
+
+      const shuffledRepos = repos.sort(() => 0.5 - Math.random());
+      const slicedRepos = shuffledRepos.slice(0, 6);
+
+      setData({
+        user,
+        repos: slicedRepos,
+      })
+    })
+  }, []);
+
+  if(data?.error) {
+    return <h1>{data.error}</h1>
+  }
+
+  if(!data?.user || !data?.repos) {
+    return <h1>Loading...</h1>
+  }
+
   const TabContent = () => (
     <div className="content">
       <RepoIcon />
       <span className="label">Repositories</span>
-      <span className="number">26</span>
+      <span className="number">{data.user?.public_repos}</span>
     </div>
   )
 
@@ -30,15 +74,15 @@ const Profile: React.FC = () => {
       <Main>
         <LeftSide>
           <ProfileData
-            username={'guilhermerodz'}
-            name={'Guilherme Rodz'}
-            avatarURL={'https://avatars0.githubusercontent.com/u/10366880?s=460&u=ae99daa8dbb052dfcd0048862ab87007f677e6ed&v=4'}
-            followers={887}
-            followwing={7}
-            company={'Rocketseat'}
-            location={'São Paulo, Brazil'}
-            email={'emailqualquer@gmail.com'}
-            blog={undefined}
+            username={data.user.login}
+            name={data.user.name}
+            avatarURL={data.user.avatar_url}
+            followers={data.user.followers}
+            followwing={data.user.following}
+            company={data.user.company}
+            location={data.user.location}
+            email={data.user.email}
+            blog={data.user.blog}
           />
         </LeftSide>
 
@@ -52,15 +96,15 @@ const Profile: React.FC = () => {
             <h2>Random repos</h2>
 
             <div>
-              {[1, 2, 3, 4, 5, 6].map(n => (
+              {data.repos.map(item => (
                 <RepoCard
-                  key={n}
-                  username={'guilhermerodz'}
-                  reponame={'youtubo-content'}
-                  description={'Contains all of my YouTube lessons code'}
-                  language={n%3 === 0 ? 'JavaScript' : 'TypeScript'}
-                  stars={8}
-                  forks={4}
+                  key={item.name}
+                  username={item.owner.login}
+                  reponame={item.name}
+                  description={item.description}
+                  language={item.language}
+                  stars={item.stargazers_count}
+                  forks={item.forks}
                 />
               ))}
             </div>
--- interface-github/src/pages/Repo/index.tsx
+++ interface-github/src/pages/Repo/index.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { Container, Breadcrumb, RepoIcon, Stats, StarIcon, ForkIcon, LinkButton, GithubIcon } from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className='username' to='/guilhermerodz' >
+          guilhermerodz
+        </Link>
+
+        <span>/</span>
+
+        <Link className='reponame' to='/guilhermerodz/youtube-content' >
+          youtube-content
+        </Link>
+      </Breadcrumb>
+
+      <p>Contains all of my YouTube lessons code.</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href='https://github.com/guilhermerodz/youtube-content' >
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
 }
 
 export default Repo;
