d2950c48d6662dbc72a4fb8c40b81d5dadd38837
Rafael <rafael-rafa2.1@hotmail.com>
2020-08-30 12:26:56 -0300

    MenuBar

--- interface-twitter/src/styles/Icons.ts
+++ interface-twitter/src/styles/Icons.ts
@@ -1,6 +1,7 @@
 export { AiOutlineHome, AiOutlineMail, AiOutlineSearch } from 'react-icons/ai';
-export { MdNotificationsNone, MdFavorite, MdCake } from 'react-icons/md';
-export { BsArrowLeft, BsChatDots } from 'react-icons/bs';
+export { MdNotificationsNone, MdFavorite, MdFavoriteBorder, MdCake } from 'react-icons/md';
+export { BsArrowLeft, BsChatDots, BsPersonFill } from 'react-icons/bs';
 export { GoLocation } from 'react-icons/go';
+export { IoMdExit } from 'react-icons/io';
 
 export { RocketseatIcon as Rocketseat } from '../components/RocketseatIcon';
