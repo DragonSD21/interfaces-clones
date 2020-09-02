7e86e35b7f20c1f1da9ae81f361b8654a088c2d4
Rafael <rafael-rafa2.1@hotmail.com>
2020-09-02 09:12:24 -0300

    Criação do projeto de interface do Discord

---
+++ interface-discord/src/components/Layout/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ServerList from '../ServerList';
+
+import { Container } from './styles';
+
+const Layout: React.FC = () => {
+  return (
+      <Container>
+          <ServerList />
+      </Container>
+  );
+}
+
+export default Layout;
