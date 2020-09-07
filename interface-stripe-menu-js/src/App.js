7839502a61c76cddaba966d765e89c9f5e903b80
Rafael <rafael-rafa2.1@hotmail.com>
2020-09-07 12:55:30 -0300

    Criação do projeto de interfaceamento do menu do Stripe

---
+++ interface-stripe-menu-js/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import GLobalStyles from './styles/GlobalStyles';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <h1>Oi</h1>
+      </Layout>
+      <GLobalStyles />
+    </>
+  );
+}
+
+export default App;
